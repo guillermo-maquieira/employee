97a8d6531b91bdc877989b26caace09488ef7bc9
Guillermo Maquieira <guimaq@gmail.com>
2024-07-16 16:57:52 -0300

    Change ping db

--- server/db/connection.js
+++ server/db/connection.js
@@ -13,7 +13,7 @@ try {
   // Connect the client to the server
   await client.connect();
   // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
+  await client.db("test").command({ ping: 1 });
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } catch (err) {
   console.error(err);
